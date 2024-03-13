014647a99b5ebced0f754f77628071809cd5ac46
rxa15 <114054405+rxa15@users.noreply.github.com>
2024-03-13 20:12:33 +0700

    [REFACTOR] Optimizing method joinStudentNames

--- src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
+++ src/main/java/com/advpro/profiling/tutorial/service/StudentService.java
@@ -57,10 +57,18 @@ public class StudentService {
 
     public String joinStudentNames() {
         List<Student> students = studentRepository.findAll();
-        String result = "";
+
+        StringBuilder result = new StringBuilder();
+        String coma = ", ";
+
         for (Student student : students) {
-            result += student.getName() + ", ";
+            result.append(student.getName()).append(coma);
         }
-        return result.substring(0, result.length() - 2);
+
+        if (!students.isEmpty()) {
+            result.setLength(result.length() - 2);
+        }
+
+        return result.toString();
     }
 }
